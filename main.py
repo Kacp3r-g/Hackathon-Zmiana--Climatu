864bb95bb13ef488b3b6f39e76986cd917fc1cc0
Kacp3r-g <kacpergorecki2023@gmail.com>
2026-04-09 13:39:57 +0100

    main.py

--- main.py
+++ main.py
@@ -3,16 +3,4 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-token = os.getenv("TOKEN")
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print("Bot is ready")
-
 bot.run(token)
